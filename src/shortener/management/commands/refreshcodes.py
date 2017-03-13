b28852904468e7f587131efa1af10ab390f5e65b
sushilthapa <sushil@spark.com.np>
2017-03-13 15:55:58 +0545

    added refresh custom command

---
+++ src/shortener/management/commands/refreshcodes.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from shortener.models import KirrURL
+
+class Command(BaseCommand):
+    help = 'refreshes all KirrURL shortcodes codes'
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument('poll_id', nargs='+', type=int)
+
+    def handle(self, *args, **options):
+        return KirrURL.objects.refresh_shortcodes()
